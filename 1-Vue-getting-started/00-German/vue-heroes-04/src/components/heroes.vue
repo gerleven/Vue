e3e02285514b5b0505bd288d8c536250d5f463ea
german.levental <german.levental@diveria.com>
2021-09-15 16:58:34 -0300

    lista de heroes agregada

--- 1-Vue-getting-started/00-German/vue-heroes-04/src/components/heroes.vue
+++ 1-Vue-getting-started/00-German/vue-heroes-04/src/components/heroes.vue
@@ -9,25 +9,25 @@
       <div class="column is-8">
         <div class="card edit-detail">
           <header class="card-header">
-            <p class="card-header-title" v-text="hero.firstName">En vez de la interpolacion con {{hero.firstName}} se puede usar v-text y esto sera ignorado</p>
+            <p class="card-header-title" v-text="selectedHero.firstName">En vez de la interpolacion con {{selectedHero.firstName}} se puede usar v-text y esto sera ignorado</p>
           </header>
           <div class="card-content">
             <div class="content">
               <div class="field">
                 <label class="label" for="id">id</label>
-                <label class="input" id="id" readonly>{{hero.id}}</label>
+                <label class="input" id="id" readonly>{{selectedHero.id}}</label>
               </div>
               <div class="field">
                 <label class="label" for="firstName">first name</label>
-                <input class="input" id="firstName" v-model="hero.firstName"/>
+                <input class="input" id="firstName" v-model="selectedHero.firstName"/>
               </div>
               <div class="field">
                 <label class="label" for="lastName">last name</label>
-                <input class="input" id="lastName" v-model="hero.lastName"/>
+                <input class="input" id="lastName" v-model="selectedHero.lastName"/>
               </div>
               <div class="field">
                 <label class="label" for="description">description</label>
-                <textarea class="input" id="description" type="text" v-model="hero.description"/>
+                <textarea class="input" id="description" type="text" v-model="selectedHero.description"/>
               </div>
 
               <!-- RADIO BUTTONS-->
@@ -36,7 +36,7 @@
               <div class="field">
                 <label class="label">cape color</label>
                 <label class="radio" for="color-red">
-                  <input type="radio" id="color-red" value="red" v-model="hero.capeColor"/>
+                  <input type="radio" id="color-red" value="red" v-model="selectedHero.capeColor"/>
                   red
                 </label>
                 <label class="radio" for="color-blue">
@@ -44,32 +44,32 @@
                     type="radio"
                     id="color-blue"
                     value="blue"
-                    v-model="hero.capeColor"
+                    v-model="selectedHero.capeColor"
                   />
                   blue
                 </label>
                 <label class="radio" for="color-green">
-                  <input type="radio" id="color-green" value="green" v-model="hero.capeColor"/>
+                  <input type="radio" id="color-green" value="green" v-model="selectedHero.capeColor"/>
                   green
                 </label>
 
                 <!--Como bindear la property style de un elemento a una clase que sea dinamica segun cambie una variable: -->
-                <div class="color-line" v-bind:style="{'background-color': hero.capeColor}"></div> <!--Aca la property style cambiara cada vez que capeColor lo haga-->
-                <div class="color-line" :style="{'background-color': hero.capeColor}"></div>
+                <div class="color-line" v-bind:style="{'background-color': selectedHero.capeColor}"></div> <!--Aca la property style cambiara cada vez que capeColor lo haga-->
+                <div class="color-line" :style="{'background-color': selectedHero.capeColor}"></div>
                 <div class="color-line" style="background-color: #FF5511"></div> <!--Aca no hay bindeo, es una asignacion directa de la property style-->
                 
                 <!--"2do radio goup: headColor-->
                 <label class="radio" for="color-silver">
-                  <input type="radio" id="color-silver" value="silver" v-model="hero.headColor"/>
+                  <input type="radio" id="color-silver" value="silver" v-model="selectedHero.headColor"/>
                   silver
                 </label>
                 
                 <label class="radio" for="color-pink">
-                  <input type="radio" id="color-pink" value="pink" v-model="hero.headColor"/> <!--aca el radio group pasa a ser hero.headCOlor-->
+                  <input type="radio" id="color-pink" value="pink" v-model="selectedHero.headColor"/> <!--aca el radio group pasa a ser selectedHero.headCOlor-->
                   pink
                 </label>
 
-                <div class="color-line" v-bind:style="{'background-color': hero.headColor}"></div>
+                <div class="color-line" v-bind:style="{'background-color': selectedHero.headColor}"></div>
                 
               </div>
 
@@ -78,7 +78,7 @@
                 <label for="power">
                   super power
                   <div class="select is-primary">
-                    <select id="power" v-model="hero.power" @keyup.esc="clearPower" :class="{invalid: hero.power===''}">
+                    <select id="power" v-model="selectedHero.power" @keyup.esc="clearPower" :class="{invalid: selectedHero.power===''}">
                       <option disabled value>Please select one</option>
                       <option>Speed</option>
                       <option>Flight</option>
@@ -91,20 +91,20 @@
               <div class="field">
                 <label class="checkbox" for="active">
                   active
-                  <input type="checkbox" class="is-primary" id="active" v-model="hero.active"/>
+                  <input type="checkbox" class="is-primary" id="active" v-model="selectedHero.active"/>
                 </label>
                 <br>
-                <!--Como bindear la property class de un elemento a una clase que cambie dinamicamente segun cambie una variable (hero.active en este caso): -->
-                <p :class="{checkedGreen: hero.active, checkedRed: !hero.active, checkedBold: hero.active, fontSize20px: true}">testeando class binding saegun el checkbox</p>
+                <!--Como bindear la property class de un elemento a una clase que cambie dinamicamente segun cambie una variable (selectedHero.active en este caso): -->
+                <p :class="{checkedGreen: selectedHero.active, checkedRed: !selectedHero.active, checkedBold: selectedHero.active, fontSize20px: true}">testeando class binding saegun el checkbox</p>
               </div>
             </div>
           </div>
           <footer class="card-footer">
-            <button class="link card-footer-item cancel-button" v-on:click="cancelHero"> <!--long syntax to handle events-->
+            <button class="link card-footer-item cancel-button" v-on:click="cancelselectedHero"> <!--long syntax to handle events-->
               <i class="fas fa-undo"></i>
               <span>Cancel</span>
             </button>
-            <button class="link card-footer-item" @click="saveHero"> <!--short syntax to handle events-->
+            <button class="link card-footer-item" @click="saveselectedHero"> <!--short syntax to handle events-->
               <i class="fas fa-save"></i>
               <span>Save</span>
             </button>
@@ -121,7 +121,7 @@ export default {
   name: 'Heroes',
   data() {
     return {
-      hero: {
+      selectedHero: {
         id: 20,
         firstName: 'Purulaio',
         lastName: 'Rwakanilloss',
@@ -132,6 +132,32 @@ export default {
         active: true,
 
       },
+      heroes: [
+        {
+          id: 10,
+          firstName: 'Ella',
+          lastName: 'Papa',
+          description: 'fashionista',
+        },
+        {
+          id: 20,
+          firstName: 'Madelyn',
+          lastName: 'Papa',
+          description: 'the cat whisperer',
+        },
+        {
+          id: 30,
+          firstName: 'Haley',
+          lastName: 'Papa',
+          description: 'pen wielder',
+        },
+        {
+          id: 40,
+          firstName: 'Landon',
+          lastName: 'Papa',
+          description: 'arc trooper',
+        },
+      ],
       mesagge: 'test'
     }
   },
