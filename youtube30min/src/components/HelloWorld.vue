6b6c7511f2e1934307f93c8c62f012e1e744abdb
gerleven <gerleven@gmail.com>
2021-09-15 09:33:25 -0300

    migro la de youtube tambien

---
+++ youtube30min/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{texto}} te desea {{autor}}</h2>
+    <miComponente></miComponente>
+    
+  </div>
+</template>
+
+<script>
+import MiComponente from './miComponente.vue'
+export default {
+  name: 'HelloWorld',
+  components: {MiComponente},
+  props: {
+    msg: String
+  },
+  data(){
+    return {
+      texto: "Hola mundo desde el componente",
+      autor: "German"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
