eec058cdd6d191ef8d9a34a5932e490ad0d5f524
german.levental <german.levental@diveria.com>
2021-10-17 02:31:48 -0300

    unidad 6 terminada 'component communication'

--- 1-Vue-getting-started/06-component-communication/begin/vue-heroes/src/components/heroes.vue
+++ 1-Vue-getting-started/06-component-communication/begin/vue-heroes/src/components/heroes.vue
@@ -31,88 +31,13 @@
           <!-- <heroes-list> -->
 
           <!-- <hero-detail> -->
-          <div v-if="selectedHero">
-            <div class="card">
-              <header class="card-header">
-                <p class="card-header-title">{{ fullName }}</p>
-              </header>
-              <div class="card-content">
-                <div class="content">
-                  <div class="field">
-                    <label class="label" for="id">id</label>
-                    <label class="input" name="id" readonly>{{
-                      selectedHero.id
-                    }}</label>
-                  </div>
-                  <div class="field">
-                    <label class="label" for="firstName">first name</label>
-                    <input
-                      class="input"
-                      name="firstName"
-                      v-model="selectedHero.firstName"
-                    />
-                  </div>
-                  <div class="field">
-                    <label class="label" for="lastName">last name</label>
-                    <input
-                      class="input"
-                      name="lastName"
-                      v-model="selectedHero.lastName"
-                    />
-                  </div>
-                  <div class="field">
-                    <label class="label" for="description">description</label>
-                    <input
-                      class="input"
-                      name="description"
-                      v-model="selectedHero.description"
-                    />
-                  </div>
-                  <div class="field">
-                    <label class="label" for="originDate">origin date</label>
-                    <input
-                      type="date"
-                      class="input"
-                      id="originDate"
-                      v-model="selectedHero.originDate"
-                    />
-                    <p class="comment">
-                      My origin story began on
-                      {{ selectedHero.originDate | shortDate }}
-                    </p>
-                  </div>
-                  <div class="field">
-                    <label class="label" for="capeCounter">cape counter</label>
-                    <input
-                      class="input"
-                      name="capeCounter"
-                      type="number"
-                      v-model="selectedHero.capeCounter"
-                    />
-                  </div>
-                  <div class="field">
-                    <label class="label" for="capeMessage">cape message</label>
-                    <label class="input" name="capeMessage">{{
-                      capeMessage
-                    }}</label>
-                  </div>
-                </div>
-              </div>
-              <footer class="card-footer">
-                <button
-                  class="link card-footer-item cancel-button"
-                  @click="cancelHero()"
-                >
-                  <i class="fas fa-undo"></i>
-                  <span>Cancel</span>
-                </button>
-                <button class="link card-footer-item" @click="saveHero()">
-                  <i class="fas fa-save"></i>
-                  <span>Save</span>
-                </button>
-              </footer>
-            </div>
-          </div>
+          <HeroDetail
+            v-if="selectedHero"
+            :hero="selectedHero"
+            @save="saveHero"
+            @cancel="cancelHero"
+          />
+
           <!-- </hero-detail> -->
 
           <div class="notification is-info" v-show="message">{{ message }}</div>
@@ -124,8 +49,10 @@
 
 <script>
 import { format } from 'date-fns';
-
-import { displayDateFormat, ourHeroes } from '../shared';
+import HeroDetail from '@/components/hero-detail';
+import { displayDateFormat, ourHeroes, logger } from '../shared';
+import { lifecycleHooks } from '../shared'; //y asis in poner el nombre del archivo que pasa si en shared ha 2 archivos que adentro tienen el mimo nombre de clase? cual de los 2 importa?
+import { heroWatchers } from '../shared/logging-mixins';
 
 export default {
   name: 'Heroes',
@@ -137,16 +64,8 @@ export default {
       capeMessage: '',
     };
   },
-  created() {
-    this.loadHeroes();
-  },
-  computed: {
-    fullName() {
-      return this.selectedHero
-        ? `${this.selectedHero.firstName} ${this.selectedHero.lastName}`
-        : '';
-    },
-  },
+  components: { HeroDetail },
+  mixins: [lifecycleHooks, heroWatchers],
   methods: {
     async getHeroes() {
       return new Promise(resolve => {
@@ -162,9 +81,9 @@ export default {
     cancelHero() {
       this.selectedHero = undefined;
     },
-    saveHero() {
-      const index = this.heroes.findIndex(h => h.id === this.selectedHero.id);
-      this.heroes.splice(index, 1, this.selectedHero);
+    saveHero(hero) {
+      const index = this.heroes.findIndex(h => h.id === hero.id);
+      this.heroes.splice(index, 1, hero);
       this.heroes = [...this.heroes];
       this.selectedHero = undefined;
     },
@@ -189,20 +108,31 @@ export default {
       }
     },
   },
+  filters: {
+    shortDate: function(value) {
+      return format(value, displayDateFormat);
+    },
+  },
+  computed: {
+    fullName() {
+      return this.selectedHero
+        ? `${this.selectedHero.firstName} ${this.selectedHero.lastName}`
+        : '';
+    },
+  },
+  created() {
+    this.loadHeroes();
+      logger.info(`${this.componentName} created a hook called `); //ese componentName es el que viene del computed del mixin
+  },
   watch: {
     'selectedHero.capeCounter': {
       immediate: true,
       handler(newValue, oldValue) {
         console.log(
-          `CapeCounter watcher evalauted. old=${oldValue}, new=${newValue}`
+          `CapeCounter watcher evalauted. old=${oldValue}, new=${newValue}`,
         );
         this.handleTheCapes(newValue);
       },
-    },
-  },
-  filters: {
-    shortDate: function(value) {
-      return format(value, displayDateFormat);
     },
   },
 };
